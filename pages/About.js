b064a0b00c68b565e9331f722fdb1250cc18f93c
pritamstech <pritamstech@hotmail.com>
2024-08-24 11:15:04 +0530

    Changes For Deployment With ESLint Rules

--- pages/About.js
+++ pages/About.js
@@ -12,14 +12,15 @@ const About = () => {
         articles, thought-provoking discussions, and a community of passionate
         technical enthusiast. Our platform is dedicated to bringing you the
         latest trends, tips, and stories from the field of Mainly
-        coding,emerging technologies, computer science, and more. Whether you're
-        looking for expert advice, personal stories, or just a good read, you'll
-        find it here. Our team of experienced bloggers and guest contributors
-        work tirelessly to provide content that is not only informative but also
-        engaging and inspiring. We believe in the power of words to connect
-        people, share knowledge, and spark creativity. Join us on this journey
-        of exploration and discovery, and become a part of our growing
-        community. Thank you for visiting, and we hope you enjoy your time here!
+        coding,emerging technologies, computer science, and more. Whether
+        you&apos;re looking for expert advice, personal stories, or just a good
+        read, you&apos;ll find it here. Our team of experienced bloggers and
+        guest contributors work tirelessly to provide content that is not only
+        informative but also engaging and inspiring. We believe in the power of
+        words to connect people, share knowledge, and spark creativity. Join us
+        on this journey of exploration and discovery, and become a part of our
+        growing community. Thank you for visiting, and we hope you enjoy your
+        time here!
       </div>
     </>
   );
